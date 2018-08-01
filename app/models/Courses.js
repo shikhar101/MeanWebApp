33650f4dd66dde8c913693229fd6a842025e50af
ALOK743165 <ALOK743165@GMAIL.COM>
2018-08-01 22:55:35 +0530

    Update Courses.js

--- app/models/Courses.js
+++ app/models/Courses.js
@@ -2,10 +2,6 @@ var mongoose=require('mongoose');
 var Schema=mongoose.Schema;
 
 var CoursesSchema = new Schema({
-    id: {
-        type: Number,
-        required: true
-    },
     Code: {
         type: Number,
         required: true
